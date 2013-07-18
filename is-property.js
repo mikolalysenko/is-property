0a85ea5b6b1264ea1cdecc6e5cf186adbb3ffc50
Mikola Lysenko <mikolalysenko@gmail.com>
2013-07-18 09:10:12 -0500

    adding files

---
+++ is-property.js
@@ -0,0 +1,5 @@
+"use strict"
+function isProperty(str) {
+  return /^[^0-9\/\\.\-+*!~()\[\];:?'"<>,{}|`~%\^& \f\n\r\t\v​\u00a0\u1680​\u180e\u2000​\u2001\u2002​\u2003\u2004​\u2005\u2006​\u2007\u2008​\u2009\u200a​\u2028\u2029​​\u202f\u205f​\u3000]\w*$/.test(str)
+}
+module.exports = isProperty
---
+++ test/test.js
@@ -0,0 +1,15 @@
+"use strict"
+var isProperty = require("../is-property.js")
+require("tape")("is-property", function(t) {
+  
+  t.assert(isProperty("foo"))
+  t.assert(!isProperty(".foo"))
+  t.assert(!isProperty("a.b.c"))
+  t.assert(isProperty("_joke"))
+  t.assert(isProperty("j_a_b_c"))
+  t.assert(isProperty("f00"))
+  t.assert(!isProperty("0bad"))
+  t.assert(isProperty("break"))
+  
+  t.end()
+})
